b4146c30c51a1603e19e04c8e250f12071a3bdd7
Sachinda Niroshan <sachiya750@gmail.com>
2025-01-27 00:21:46 +0530

    Initial commit

---
+++ src/main/java/com/spring_jwt_advanced/spring_jwt_advanced/SpringJwtAdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.spring_jwt_advanced.spring_jwt_advanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtAdvancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtAdvancedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_jwt_advanced/spring_jwt_advanced/SpringJwtAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_jwt_advanced.spring_jwt_advanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtAdvancedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
